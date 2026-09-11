92d14027ed6a657e7079021df4b01aa76606d7d1
vayavedant2 <vayavedant2@gmail.com>
2026-09-11 16:46:18 +0530

    ui changes

--- assets/js/config/assets.js
+++ assets/js/config/assets.js
@@ -2,20 +2,16 @@ export const ASSETS = {
   hero: "https://framerusercontent.com/images/Ngp46Al0G5CMdaGWyMnjewOlT0A.png",
   noise: "https://framerusercontent.com/images/WXwwArEbCEXxVkbbiaI5FD2o.png",
   introPortrait:
-    "https://framerusercontent.com/images/uwPmCBJ07EBXYM4euDaXCTYD5c.png",
+    "https://framerusercontent.com/images/8vEcbr13XA8UqEzxmpiOczUxYa0.png",
   works: {
-    braven:
+    startupsTech:
       "https://framerusercontent.com/images/CAN9PHdASNma7CJYzTNbxruDWI.jpg",
-    lumina:
+    fashionBeautyD2c:
       "https://framerusercontent.com/images/Leb3O5uU3RnwMnpbg7dhTLW92y8.jpg",
-    goldie:
+    creatorsArtistsCulture:
       "https://framerusercontent.com/images/5vlIf4i2WSxepjiGKiKbsxVimY.jpg",
-    sophia:
+    foundersExecutives:
       "https://framerusercontent.com/images/qA0wPQk6BcL1aflpa8yglw1jl4w.jpg",
-    zolly:
-      "https://framerusercontent.com/images/fH5vANvbQe6CygkZPuccQudtJys.jpg",
-    flora:
-      "https://framerusercontent.com/images/GHxPfVRGzd31817k26SURNx7oo.jpg",
   },
   testimonial:
     "https://framerusercontent.com/images/antmDkZCNxxn6lFKhbu3P1xtumY.png",
